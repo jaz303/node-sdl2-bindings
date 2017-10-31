6dcc696e916dd0af9080a665deb8d599a4bef236
Jason Frame <jason@onehackoranother.com>
2017-10-31 22:55:58 +0000

    add joystick shims to track currently open joysticks

--- index.js
+++ index.js
@@ -1,2 +1,28 @@
 const sdl = require('./build/Release/sdl');
+
+const _joystickOpen = sdl.joystickOpen;
+const _joystickClose = sdl.	joystickClose;
+
+const joystickMap = {};
+
+sdl.joystickOpen = function(index) {
+	const js = _joystickOpen(index);
+	joystickMap[sdl.joystickInstanceID(js)] = js;
+	return js;
+}
+
+sdl.joystickClose = function(js) {
+	const instanceId = sdl.joystickInstanceID(js);
+	delete joystickMap[instanceId];
+	_joystickClose(js);
+}
+
+sdl.joystickFromInstanceID = function(instanceId) {
+	const js = joystickMap[instanceId];
+	if (!js) {
+		throw new Error("unknown joystick ID: " + instanceId);
+	}
+	return js;
+}
+
 module.exports = sdl;
